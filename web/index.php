6abb2c914228efc2682c3fc22fb457337dfd65d7
Jorge Lillo <jorge.lillo9@gmail.com>
2016-03-10 19:05:00 +0100

    Adapted andia-agency-v2-1 template with twig templates

--- web/index.php
+++ web/index.php
@@ -19,7 +19,37 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return $app['twig']->render('index.twig', array('activeTab' => 0));
+});
+
+$app->get('/alt', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index2.twig', array('activeTab' => 0));
+});
+
+$app->get('/portfolio', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('portfolio.twig', array('activeTab'=> 1));
+});
+
+$app->get('/services', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('services.twig', array('activeTab'=> 2));
+});
+
+$app->get('/about', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('about.twig', array('activeTab'=> 3));
+});
+
+$app->get('/contact', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('contact.twig', array('activeTab'=> 4));
+});
+
+$app->get('/pricing', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('pricing-tables.twig', array('activeTab'=> 5));
 });
 
 $app->run();
